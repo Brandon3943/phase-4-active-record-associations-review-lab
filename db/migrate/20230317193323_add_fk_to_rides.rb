bac7a2598b3870a161c130349329181f0cc612a3
root <root@Ty-Laptop.localdomain>
2023-03-17 15:49:06 -0400

    completed assignment

---
+++ db/migrate/20230317193323_add_fk_to_rides.rb
@@ -0,0 +1,6 @@
+class AddFkToRides < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :rides, :passenger, foreign_key: true 
+    add_reference :rides, :taxi, foreign_key: true 
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p = Passenger.create
+t = Taxi.create
+
+
+puts "done seeding"
